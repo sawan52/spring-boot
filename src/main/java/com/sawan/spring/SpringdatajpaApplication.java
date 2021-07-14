9a0a8a3086f9193468413ccbe229005ee3aeafcf
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-14 16:02:41 +0530

    created restful web services and perform CRUD operations

--- src/main/java/com/sawan/spring/SpringdatajpaApplication.java
+++ src/main/java/com/sawan/spring/SpringdatajpaApplication.java
@@ -1,4 +1,4 @@
-package com.sawan.spring.data;
+package com.sawan.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/sawan/spring/controllers/ProductController.java
@@ -0,0 +1,48 @@
+package com.sawan.spring.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sawan.spring.data.entities.Product;
+import com.sawan.spring.data.repo.ProductRepo;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	ProductRepo repository;
+
+	@GetMapping
+	public Iterable<Product> getAllProducts() {
+		return repository.findAll();
+	}
+
+	@PostMapping
+	public Product create(@RequestBody Product product) {
+		return repository.save(product);
+	}
+
+	@PutMapping
+	public Product update(@RequestBody Product product) {
+		return repository.save(product);
+	}
+
+	@GetMapping("/{id}")
+	public Product getProductById(@PathVariable("id") Long id) {
+		Product prod = null;
+		Optional<Product> optional = repository.findById(id);
+		if (optional.isPresent()) {
+			prod = optional.get();
+		}
+		return prod;
+	}
+}
