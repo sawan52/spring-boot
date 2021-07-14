88a7108617b5e1274aaa68e2f1d01e2a95b58e80
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-14 14:49:02 +0530

    created spring boot web project, created a controller and run the application

---
+++ src/main/java/com/sawan/spring/boot/web/SpringbootwebApplication.java
@@ -0,0 +1,13 @@
+package com.sawan.spring.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sawan/spring/boot/web/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.sawan.spring.boot.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello(@RequestParam String name) {
+		return "Hello " + name;
+	}
+
+}
