46b4cb07dbda93c50c588056ea7807722b3c809f
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-23 17:01:44 +0530

    created all the CRUD methods in controller class

--- src/main/java/com/sawan/springweb/controllers/ProductRestController.java
+++ src/main/java/com/sawan/springweb/controllers/ProductRestController.java
@@ -3,6 +3,7 @@ package com.sawan.springweb.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,28 @@ public class ProductRestController {
 	ProductRepository repository;
 
 	@RequestMapping(value = "/products/", method = RequestMethod.GET)
-	public List<Product> getProducts() {
+	public List<Product> getAllProducts() {
 		return repository.findAll();
 	}
+
+	@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+	public Product getproduct(@PathVariable("id") int id) {
+		return repository.findById(id).get();
+	}
+
+	@RequestMapping(value = "/products/", method = RequestMethod.POST)
+	public Product createProduct(Product product) {
+		return repository.save(product);
+	}
+
+	@RequestMapping(value = "/products/", method = RequestMethod.PUT)
+	public Product updateProduct(Product product) {
+		return repository.save(product);
+	}
+
+	@RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+	public void deleteProduct(@PathVariable("id") int id) {
+		repository.deleteById(id);
+	}
+
 }
