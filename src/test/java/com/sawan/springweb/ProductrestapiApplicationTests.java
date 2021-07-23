475e38374bb2513bc821c29ff610467a75e1d887
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-23 15:15:58 +0530

    initial commit

---
+++ src/test/java/com/sawan/springweb/ProductrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawan.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
