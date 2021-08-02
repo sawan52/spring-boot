f2a7d9b9b37e08c4438dd8a8212f1509542a9d29
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-02 17:12:07 +0530

    implemented spring java messaging service (JMS)

---
+++ src/main/java/com/sawan/springboot/MessageSender.java
@@ -0,0 +1,23 @@
+package com.sawan.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Value("${springjms.myQueue}")
+	private String queue;
+
+	public void send(String message) {
+		System.out.println("Message Sent ===> " + message);
+		jmsTemplate.convertAndSend(queue, message);
+	}
+
+}
---
+++ src/test/java/com/sawan/springboot/SpringjmsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sawan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjmsApplicationTests {
+
+	@Autowired
+	private MessageSender sender;
+
+	@Test
+	void testSendAndReceive() {
+		sender.send("Hello Spring JMS");
+	}
+
+}
